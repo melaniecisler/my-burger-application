5184ce38be1045b7af9a99047f1b9914408c73cc
Melanie Cisler <65982774+melaniecisler@users.noreply.github.com>
2020-10-12 14:01:59 -0500

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection =  mysql.createConnection({
         host: "localhost",
-        port: 3306,
+        //port: 3306,
         database: "burger_db",
         user: "root",
         password: "TitoBurrito1!"
     });
-
+};
+connection.connect();
 module.exports = connection;
