fa5e67fb1b8e69b08ab3d4827f8d2f8e33ef4fc2
Melanie Cisler <65982774+melaniecisler@users.noreply.github.com>
2020-10-12 16:59:28 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,11 @@
 const mysql = require("mysql");
 
-let connection;
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection =  mysql.createConnection({
+let connection =  mysql.createConnection({
         host: "localhost",
-        //port: 3306,
+        port: 3306,
         database: "burger_db",
         user: "root",
         password: ""
     });
-};
-connection.connect();
+
 module.exports = connection;
